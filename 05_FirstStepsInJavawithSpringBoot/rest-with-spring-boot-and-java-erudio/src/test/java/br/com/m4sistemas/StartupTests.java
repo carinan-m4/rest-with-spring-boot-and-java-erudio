eaecc8200985a0c3b5984a7188ad5ff75d86f37a
carina <carina.nevreden@m4sistemas.com.br>
2024-09-05 17:25:16 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/m4sistemas/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.m4sistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
